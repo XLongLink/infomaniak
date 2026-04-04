82def9e6581fbd9d937c1d28a68b2dfb97a2f3a6
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2026-04-04 23:16:43 +0200

    Implement public cloud config get endpoint

--- infomaniak/models/cloud/__init__.py
+++ infomaniak/models/cloud/__init__.py
@@ -1,9 +1,13 @@
+from infomaniak.models.cloud.config import (PublicCloudConfig,
+                                            PublicCloudConfigResponse)
 from infomaniak.models.cloud.projects import (
     PublicCloudProject, PublicCloudProjectTag, PublicCloudProjectListResponse,
     CreatePublicCloudProjectResponse, PublicCloudProjectInvitationResponse,
     PublicCloudProjectAsyncActionResponse)
 
 __all__ = [
+    "PublicCloudConfig",
+    "PublicCloudConfigResponse",
     "PublicCloudProjectTag",
     "PublicCloudProject",
     "PublicCloudProjectAsyncActionResponse",
