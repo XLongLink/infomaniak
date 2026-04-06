362bc13084788e578267ec8ce30b64598d00914c
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2026-04-06 21:35:45 +0200

    Implement core.actions.list and document core actions

--- infomaniak/models/__init__.py
+++ infomaniak/models/__init__.py
@@ -1,7 +1,7 @@
 """SDK models."""
 
 from infomaniak.models.dns import DNSZone, DNSRecord
-from infomaniak.models.core import Timezone
+from infomaniak.models.core import Action, Timezone
 from infomaniak.models.cloud import (PublicCloudProject, PublicCloudProjectTag,
                                      PublicCloudProjectListResponse,
                                      CreatePublicCloudProjectResponse)
